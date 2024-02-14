aaa68633fda36c2f6e786374dbf3dce24d5e30e2
Niharika Shah <shahniharikagujju@gmail.com>
2024-02-15 01:35:54 +0530

    second change

---
+++ src/components/HamburgerMenu.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './HamburgerMenu.css'; // Import your CSS file
+
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,26 +3,37 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 import logoImage from './wave.jpg';
 
-const NavBar = ({scrollToHowItWorks, toggleFAQ}) => {
+const NavBar = ({ scrollToHowItWorks, toggleFAQ }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           <img src={logoImage} alt="OceanGuardiansLogo" />
         </Link>
-        <ul className="navbar-menu">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/donate">Donate</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-          <li><Link to="/inquiries">Press Inquiries</Link></li>
-          <li><Link to="/faq" onClick={toggleFAQ}>FAQ</Link></li>
-          <li><Link to="/leaderboard">Leaderboard</Link></li>
-          <li><Link to="/how-it-works" onClick={scrollToHowItWorks}>How It Works</Link></li>
-          {/* Add more navigation items as needed */}
-        </ul>
         <span className="cursive-text">#OceanGuardians</span>
+        <li className="login"><Link to="/login">Login</Link></li>
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          <div className={`line ${isOpen ? 'open' : ''}`}></div>
+          <div className={`line ${isOpen ? 'open' : ''}`}></div>
+          <div className={`line ${isOpen ? 'open' : ''}`}></div>
+        </div>
+        
+        {isOpen && (
+          <ul className="navbar-menu">
+            <li><Link to="/shop">Shop</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+            <li><Link to="/inquiries">Press Inquiries</Link></li>
+            <li><Link to="/faq" onClick={toggleFAQ}>FAQ</Link></li>
+            <li><Link to="/leaderboard">Leaderboard</Link></li>
+            <li><Link to="/how-it-works" onClick={scrollToHowItWorks}>How It Works</Link></li>
+          </ul>
+        )}
       </div>
     </nav>
   );
