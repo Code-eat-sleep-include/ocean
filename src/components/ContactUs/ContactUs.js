174c7bfd910eb627b8a66fa64dcc8fe8288437b5
Lawful2002 <sharshvardhan101@gmail.com>
2024-02-21 01:59:04 +0530

    fixed contact us UI

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,47 +1,79 @@
 import React from 'react';
+import './ContactUs.css';
 
 const ContactSection = () => {
   return (
-    <div className="bg-info contact4 overflow-hidden position-relative">
-      {/* Row */}
-      <div className="row no-gutters">
-        <div className="container4">
-          <div className="col-lg-6 contact-box mb-4 mb-md-0">
-            <div>
-              <h1 className="title font-weight-light text-white mt-2">Contact Us</h1>
-              <form className="mt-3">
-                <div className="row">
-                  <div className="col-lg-6">
-                    <div className="form-group mt-2">
-                      <input className="form-control text-white" type="text" placeholder="name" />
-                    </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group mt-2">
-                      <input className="form-control text-white" type="email" placeholder="email address" />
-                    </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group mt-2">
-                      <textarea className="form-control text-white" rows="3" placeholder="message"></textarea>
-                    </div>
-                  </div>
-                  <div className="col-lg-6 d-flex align-items-center mt-2">
-                    <button type="submit" className="btn bg-white text-inverse px-3 py-2"><span> Submit</span></button>
-                    <span className="ml-auto text-white align-self-center"><i className="icon-phone mr-2"></i>251 546 9442</span>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-6 right-image p-r-0">
+    // <div className="bg-info contact4 overflow-hidden position-relative">
+    //   {/* Row */}
+    //   <div className="row no-gutters">
+    //     <div className="container4">
+    //       <div className="col-lg-6 contact-box mb-4 mb-md-0">
+    //         <div>
+    //           <h1 className="title font-weight-light text-white mt-2">Contact Us</h1>
+    //           <form className="mt-3">
+    //             <div className="row">
+    //               <div className="col-lg-6">
+    //                 <div className="form-group mt-2">
+    //                   <input className="form-control text-white" type="text" placeholder="name" />
+    //                 </div>
+    //               </div>
+    //               <div className="col-lg-6">
+    //                 <div className="form-group mt-2">
+    //                   <input className="form-control text-white" type="email" placeholder="email address" />
+    //                 </div>
+    //               </div>
+    //               <div className="col-lg-6">
+    //                 <div className="form-group mt-2">
+    //                   <textarea className="form-control text-white" rows="3" placeholder="message"></textarea>
+    //                 </div>
+    //               </div>
+    //               <div className="col-lg-6 d-flex align-items-center mt-2">
+    //                 <button type="submit" className="btn bg-white text-inverse px-3 py-2"><span> Submit</span></button>
+    //                 <span className="ml-auto text-white align-self-center"><i className="icon-phone mr-2"></i>251 546 9442</span>
+    //               </div>
+    //             </div>
+    //           </form>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="col-lg-6 right-image p-r-0">
+    //       <iframe
+    //         title="Google Maps"
+    //         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1619902.0054433027!2d-122.68851282163044!3d37.534535608111824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sin!4v1507725785789"
+    //         width="100%" height="538" frameBorder="0" style={{ border: 0 }} allowFullScreen
+    //         data-aos="fade-left" data-aos-duration="3000"
+    //       ></iframe>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className='outer mt-5 p-5'>
+      <div className='heading'>Contact Us</div>
+      <div className='outer-container'>
+        <div className='map-container mx-5'>
           <iframe
+            className='map-frame'
             title="Google Maps"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1619902.0054433027!2d-122.68851282163044!3d37.534535608111824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sin!4v1507725785789"
-            width="100%" height="538" frameBorder="0" style={{ border: 0 }} allowFullScreen
             data-aos="fade-left" data-aos-duration="3000"
           ></iframe>
+        </div>
+        <div className='form-container mx-5'>
+          <form>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Name</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John Doe"></input>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="johndoe@email.com"></input>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button className='btn btn-outline-primary'>Submit</button>
+          </form>
         </div>
       </div>
     </div>
