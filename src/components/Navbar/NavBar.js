d2e94af0e147d3548d5c095fe2fb3288b021965e
Niharika Shah <shahniharikagujju@gmail.com>
2024-02-21 22:59:53 +0530

    saving prev without blog

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -11,30 +11,6 @@ const NavBar = ({ scrollToHowItWorks, toggleFAQ }) => {
   };
 
   return (
-    // <nav className="navbar">
-    //   <div className="navbar-container">
-    //     <Link to="/" className="navbar-logo">
-    //       <img src={logoImage} alt="OceanGuardiansLogo" />
-    //     </Link>
-    //     <span className="cursive-text">#OceanGuardians</span>
-    //     <li className="login"><Link to="/login">Sign Up/In</Link></li>
-    //     <div className="hamburger-menu" onClick={toggleMenu}>
-    //       <div className={`line ${isOpen ? 'open' : ''}`}></div>
-    //       <div className={`line ${isOpen ? 'open' : ''}`}></div>
-    //       <div className={`line ${isOpen ? 'open' : ''}`}></div>
-    //     </div>
-    //     {isOpen && (
-    //       <ul className="navbar-menu">
-    //         <li><Link to="/shop">Shop</Link></li>
-    //         <li><Link to="/contact">Contact Us</Link></li>
-    //         <li><Link to="/inquiries">Press Inquiries</Link></li>
-    //         <li><Link to="/faq" onClick={toggleFAQ}>FAQ</Link></li>
-    //         <li><Link to="/leaderboard">Leaderboard</Link></li>
-    //         <li><Link to="/how-it-works" onClick={scrollToHowItWorks}>How It Works</Link></li>
-    //       </ul>
-    //     )}
-    //   </div>
-    // </nav>
     <nav class="navbar navbar-dark bg-dark fixed-top">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
--- src/components/PressInquiries/PressInquiries.js
+++ src/components/PressInquiries/PressInquiries.js
@@ -1,57 +1,82 @@
-import React, { useState } from 'react';
+/*import React, { useEffect } from 'react';
+import './PressInquiries.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from 'bootstrap';
+import blog3 from '../assets/blog3.jpg';
+import blog1 from '../assets/blog1.webp';
+import blog2 from '../assets/blog2.jpg';
+import blog4 from '../assets/blog4.jpg';
+import blog5 from '../assets/blog5.jpg';
+import blog6 from '../assets/blog6.jpg';
 
 const PressInquiries = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    company: '',
-    email: '',
-    phone: '',
-    message: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Press Inquiry Form submitted:', formData);
-  };
+  useEffect(() => {
+    const carouselElement = document.getElementById('carouselExampleAutoplaying');
+    const carousel = new Carousel(carouselElement, {
+      interval: 2000,
+    });
+    return () => {
+      carousel.dispose();
+    };
+  }, []);
 
   return (
-    <div>
-      <h2>Press Inquiries</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Company:
-          <input type="text" name="company" value={formData.company} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="email" name="email" value={formData.email} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Phone:
-          <input type="tel" name="phone" value={formData.phone} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Message:
-          <textarea name="message" value={formData.message} onChange={handleChange} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+    <div className='outer-press-inq mt-5 p-5'>
+      <div className='title mb-5 mx-5'>Post Your Blog</div>
+      <div className='outer-container-press-inq'>
+        <div className='blog-container mx-5'>
+          <form>
+            <div class="my-3">
+              <label for="exampleFormControlInput1" class="form-label">Name</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John Doe"></input>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="johndoe@email.com"></input>
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label">Your Blog</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button className='btn btn-primary mb-4'>Post</button>
+          </form>
+        </div>
+        <div className='yt-container mx-5'>
+          <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={blog3} className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={blog1} className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={blog2} className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={blog4} className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={blog5} className="d-block w-100" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src={blog6} className="d-block w-100" alt="..." />
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default PressInquiries;
+*/
