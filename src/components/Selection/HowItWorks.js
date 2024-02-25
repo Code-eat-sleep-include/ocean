cf75281c741b11410dfd05e60a91430726017ce6
Niharika Shah <shahniharikagujju@gmail.com>
2024-02-25 23:47:53 +0530

    final

--- src/components/Selection/HowItWorks.js
+++ src/components/Selection/HowItWorks.js
@@ -4,7 +4,7 @@ const HowItWorks = () => {
   return (
     <div id="how-it-works">
       <h2>HOW IT WORKS</h2>
-      <p>#TeamSeas will be one of the biggest, baddest, most-impactful cleanup projects of all time—and here’s how we’re doing it. Scroll on to see our cutting-edge river Interceptors, info on locally-organized cleanups, ghost-gear removal efforts and professional expeditions to areas where we can have the greatest conservation impact!</p>
+      <p>#OceanGaurdians will be one of the biggest, baddest, most-impactful cleanup projects of all time—and here’s how we’re doing it. Scroll on to see our cutting-edge river Interceptors, info on locally-organized cleanups, ghost-gear removal efforts and professional expeditions to areas where we can have the greatest conservation impact!</p>
     </div>
   );
 };
